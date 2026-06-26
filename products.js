f9cd97cd5944957775708d179b59a30b904890af
ItayU15 <itayusbiat@gmail.com>
2026-06-26 16:48:03 +0300

    Modify product names and add new product
    Updated product name for 'prod_02' and added new product 'prod_05'.

---
+++ products.js
@@ -0,0 +1,47 @@
+const catalogProducts = [
+  {
+    id: "prod_01",
+    name: "דרקון מפרקי גמיש",
+    category: "חלקים נעים",
+    price: 45,
+    description: "מודל זז המודפס ביחידה אחת ללא הרכבה, מעולה להפגת מתחים",
+    image: "images/dragon.jpg",
+    inStock: true
+  },
+  {
+    id: "prod_02",
+    name: "קליקר ניגירי",
+    category: "קליקרים",
+    price: 20,
+    description: "גאדג'ט לחיצות ממכר שמתיישב בול על כף היד",
+    image: "images/clicker.jpg",
+    inStock: true
+  },
+  {
+    id: "prod_03",
+    name: "מחזיק מפתחות עם שם אישי",
+    category: "מחזיקי מפתחות",
+    price: 25,
+    description: "הדפסה אישית של השם שלך באנגלית או בעברית",
+    image: "images/keychain.jpg",
+    inStock: true
+  },
+  {
+    id: "prod_04",
+    name: "שלט לוגו למשרד",
+    category: "מוצרים בעיצוב אישי",
+    price: 80,
+    description: "שלח לנו קובץ או תמונה ונבנה לך מודל תלת מימד ייעודי",
+    image: "images/custom_logo.jpg",
+    inStock: true
+  }
+];
+  {
+    id: "prod_05",
+    name: "קליקר המבורגר",
+    category: "קליקרים",
+    price: 10,
+    description: "גאדג'ט לחיצות ממכר שמתיישב בול על כף היד",
+    image: "images/clicker.jpg",
+    inStock: true
+  },
